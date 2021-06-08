460d0b7b41ce5834846980a18f06df95a2328349
Claudio Luis <delpuerto.programador@gmail.com>
2021-06-08 09:39:10 -0300

    Segundo Cambio para probar github

--- jaguarete/jaguareteAPP/views.py
+++ jaguarete/jaguareteAPP/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Definicion de Vistas.
 def index(request):
     return HttpResponse("hola Mundo!")
 
